a92d9d7ed6516e2e2dd759dfe67f94bee4ef4eb7
Skoryts <skoryts@gmail.com>
2018-10-05 14:54:50 +0300

    [#2] Configure webpack and typescript

--- web-app/webpack.config.js
+++ web-app/webpack.config.js
@@ -1,22 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ]
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  mode: 'development',
+  entry: './web-app/src/index.tsx',
+  devtool: 'inline-source-map',
+  //TODO: Can't config dev-server "Module not found: Error: Can't resolve './src' in '/home/airmap/Projects/Fit-or-Dead'"
+  devServer: {
+    publicPath: path.resolve(__dirname, 'web-app')
+  },
+  module: {
+    rules: [
+      {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
